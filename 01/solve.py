b2a7ff78eca88c9f055c9377e2e4daf16ead98a0
Daniel Tan <dtan@tripadvisor.com>
2018-12-05 16:31:11 +0800

    completed day1 part2

--- 01/solve.py
+++ 01/solve.py
@@ -1,18 +1,47 @@
+# Part 1
 #%%
 def calc(freq_list):
-    frequency=0
+    frequency = 0
 
     for i in freq_list:
-        operand=i[0]
-        number=int(i[1:])
+        operand = i[0]
+        number = int(i[1:])
 
-        if operand=='+':
-            frequency+=number
-        elif operand=='-':
-            frequency-=number
-    
+        if operand == "+":
+            frequency += number
+        elif operand == "-":
+            frequency -= number
+
     return frequency
 
+
 #%%
-with open('input.txt','r') as f:
-    puzzle_input=f.read().splitlines()
+with open("input.txt", "r") as f:
+    puzzle_input = f.read().splitlines()
+print(f"Part 1 answer: {calc(puzzle_input)}")
+
+# Part 2
+#%%
+from itertools import cycle
+
+
+def calc_repeat(freq_list):
+    frequency = 0
+    seen = {0}
+    for i in cycle(freq_list):
+        operand = i[0]
+        number = int(i[1:])
+        if operand == "+":
+            frequency += number
+        elif operand == "-":
+            frequency -= number
+
+        if frequency in seen:
+            return frequency
+        seen.add(frequency)
+
+
+#%%
+with open("input.txt", "r") as f:
+    puzzle_input = f.read().splitlines()
+print(f"Part 2 answer: {calc_repeat(puzzle_input)}")
--- 01/test.py
+++ 01/test.py
@@ -1,20 +1,41 @@
 import unittest
-from solve import calc
+from solve import calc, calc_repeat
+
 
 class TestCases(unittest.TestCase):
     """Tests for Day 01"""
 
+    # Part 1
     def test1(self):
-        test_input=['+1', '+1', '+1']
-        self.assertEqual(calc(test_input),3)
+        test_input = ["+1", "+1", "+1"]
+        self.assertEqual(calc(test_input), 3)
 
     def test2(self):
-        test_input=['+1', '+1', '-2']
-        self.assertEqual(calc(test_input),0)
-    
-    def test3(self):
-        test_input=['-1', '-2', '-3']
-        self.assertEqual(calc(test_input),-6)
+        test_input = ["+1", "+1", "-2"]
+        self.assertEqual(calc(test_input), 0)
 
-if __name__=="__main__":
+    def test3(self):
+        test_input = ["-1", "-2", "-3"]
+        self.assertEqual(calc(test_input), -6)
+
+    # Part 2
+    def test4(self):
+        test_input = ["+1", "-1"]
+        self.assertEqual(calc_repeat(test_input), 0)
+
+    def test5(self):
+        test_input = ["+3", "+3", "+4", "-2", "-4"]
+        self.assertEqual(calc_repeat(test_input), 10)
+
+    def test6(self):
+        test_input = ["-6", "+3", "+8", "+5", "-6"]
+        self.assertEqual(calc_repeat(test_input), 5)
+
+    def test7(self):
+        test_input = ["+7", "+7", "-2", "-7", "-4"]
+        self.assertEqual(calc_repeat(test_input), 14)
+
+
+if __name__ == "__main__":
     unittest.main()
+
