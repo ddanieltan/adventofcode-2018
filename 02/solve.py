e575e88cc04b167f30c78e6b258f6a3fe5480bca
Daniel Tan <dtan@tripadvisor.com>
2018-12-06 15:43:06 +0800

    solved day2 part2 + added tests

--- 02/solve.py
+++ 02/solve.py
@@ -1,6 +1,7 @@
 #%%
 # Part 1
 from collections import Counter
+from itertools import combinations
 
 
 def solve1(input_list):
@@ -15,3 +16,50 @@ def solve1(input_list):
             three_counter += 1
 
     return two_counter * three_counter
+
+
+#%%
+def compare(a, b):
+    """Given 2 words, return the num of different letters 
+    and the different letters"""
+
+    # check if words are same length
+    if len(a) != len(b):
+        return "Words not same length"
+
+    num_of_diffs = 0
+    diff_letters = []
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            diff_letters.append(a[i])
+        else:
+            num_of_diffs += 1
+
+    return num_of_diffs, diff_letters
+
+
+def solve2(input_list):
+
+    combis = combinations(input_list, 2)
+
+    for (a, b) in combis:
+        num_of_diffs, diff_letters = compare(a, b)
+        if num_of_diffs == 1:
+            return "".join(diff_letters)
+
+    return "No solution found"
+
+
+#%%
+compare("abcd", "abcc")
+
+#%%
+input_list = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+solve2(input_list)
+
+#%%
+with open("input.txt", "r") as f:
+    puzzle_input = f.read().splitlines()
+print(f"Part 1 solution: {solve1(puzzle_input)}")
+print(f"Part 2 solution: {solve2(puzzle_input)}")
+
--- 02/solve_test.py
+++ 02/solve_test.py
@@ -1,4 +1,4 @@
-from solve import solve1
+from solve import *
 
 
 def test_identify_2_repeat_letters():
@@ -12,4 +12,17 @@ def test_identify_2_repeat_letters():
 def test_part1_checksum():
     input_list = ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
     assert solve1(input_list) == 12
+
+
+def test_part2():
+    input_list = input_list = [
+        "abcde",
+        "fghij",
+        "klmno",
+        "pqrst",
+        "fguij",
+        "axcye",
+        "wvxyz",
+    ]
+    assert solve2(input_list) == "fgij"
 
