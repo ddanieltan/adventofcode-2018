dafde74ccf30cc558127fc20d18f9b89384f56f7
Daniel Tan <dtan@tripadvisor.com>
2019-01-04 16:36:07 +0800

    finally understood joelgrus solution

--- 07/solve.py
+++ 07/solve.py
@@ -14,26 +14,44 @@ def parse_instruction(line):
 
 
 #%%
-raw = """Step C must be finished before step A can begin.
-Step C must be finished before step F can begin.
-Step A must be finished before step B can begin.
-Step A must be finished before step D can begin.
-Step B must be finished before step E can begin.
-Step D must be finished before step E can begin.
-Step F must be finished before step E can begin.
-"""
-
-
 def preconditions(lines):
     preconditions = defaultdict(list)
+    remaining_steps = set()
     for line in lines.splitlines():
         before, after = parse_instruction(line)
+        remaining_steps.add(before)
+        remaining_steps.add(after)
         preconditions[after].append(before)
+
+    for k, _ in preconditions.items():
+        remaining_steps.remove(k)
+
+    for step in remaining_steps:
+        preconditions[step]
+
     return preconditions
 
 
+#%%
 def find_order(lines):
     preconds = preconditions(lines)
+    final_order = []
+    while preconds:  # while there are items in the defaultdict
+        candidates = [
+            step  # give the step
+            for step, reqs in preconds.items()  # tuple unpacking
+            if not reqs  # if there are no prior requirements ie. value of said key is an empty list
+        ]
+        next_item = min(candidates)
+        final_order.append(next_item)
 
-    while preconds:
-        candidates = []
+        # now remove the next_item as a requirement for all other steps
+        for reqs in preconds.values():
+            if next_item in reqs:
+                reqs.remove(next_item)
+
+        # remove next_item as a key from the defaultdict
+        del preconds[next_item]
+
+    return "".join(final_order)
+
--- 07/solve_test.py
+++ 07/solve_test.py
@@ -1,7 +1,19 @@
-from solve import parse_instruction
+from solve import parse_instruction, find_order
 
 
 def test_parse_one_line():
     line = "Step C must be finished before step F can begin."
     assert parse_instruction(line) == ("C", "F")
+
+
+def test_find_order():
+    raw = """Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
+"""
+    assert find_order(raw) == "CABDFE"
 
